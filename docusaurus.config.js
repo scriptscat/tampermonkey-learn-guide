e25cb3bb84117f345511a0fb33fadc27f09734da
lihengdao <4548212@qq.com>
2023-06-25 20:52:41 +0800

    新增sitemap生成

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        sitemap: {
+          changefreq: "weekly",
+          filename: "sitemap.xml",
+        },
       }),
     ],
   ],
