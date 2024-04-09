7a09245047b3eb683d933e953db21c13cecf8d3f
lihengdao <4548212@qq.com>
2024-04-09 10:03:56 +0800

    新增CommonJS以及ES6模块化内容及依赖更新

---
+++ demo/ES6/index.js
@@ -0,0 +1,10 @@
+import { name } from './person.js';
+import { name as liName } from './person.js';
+import  * as person  from './person.js';
+import  deafultExport  from './person.js';
+//import deafultExport, { name } from './person.js';
+
+console.log(name)
+console.log(liName)
+console.log(person.name)
+console.log(deafultExport)
